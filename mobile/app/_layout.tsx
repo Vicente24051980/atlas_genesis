94952b2f2a2e4a367afaa34cbb35aa262e30cf56
Vicente24051980 <vicentebellverfrances@gmail.com>
2026-08-20 12:45:39 +0200

    fix(mobile): satisfy Expo StatusBar typecheck

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,7 +6,7 @@ import { TerminalShell } from '../core/ui/TerminalShell';
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" backgroundColor="#030506" />
+      <StatusBar style="light" />
       <TerminalShell>
         <Slot />
       </TerminalShell>
