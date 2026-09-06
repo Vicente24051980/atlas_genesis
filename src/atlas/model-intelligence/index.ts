29edfc9199e9eb132d03e19050471e7502439eab
Vicente24051980 <vicentebellverfrances@gmail.com>
2026-09-06 23:49:21 +0200

    feat(model-intelligence): export execution and learning layers

--- src/atlas/model-intelligence/index.ts
+++ src/atlas/model-intelligence/index.ts
@@ -3,3 +3,5 @@ export * from "./scoring";
 export * from "./circuit-breaker";
 export * from "./router";
 export * from "./provider-registry";
+export * from "./execution";
+export * from "./learning";
